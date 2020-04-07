69421daf99755dbcf1c3a55fc3cbc3a53d8bf0c3
Luke Frisken <l.frisken@gmail.com>
2020-04-07 17:54:45 +0400

    fix typo in about message

--- src/main.rs
+++ src/main.rs
@@ -37,7 +37,7 @@ need to have the following gettext tools installed: \"msgcat\", \
 the \"xtr\" tool installed, which can be installed using \"cargo \
 install xtr\".
 
-You can the \"i18n-embed\" library to conventiently embed the \
+You can the \"i18n-embed\" library to conveniently embed the \
 localizations inside your application.",
         short_about()
     )
