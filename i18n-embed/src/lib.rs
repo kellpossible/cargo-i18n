910fbdf3da8501c099cb78a8c7b156475db3659c
Luke Frisken <l.frisken@gmail.com>
2022-02-10 18:08:52 +0400

    Use conditional compilation correctly for i18n-embed doctests

--- i18n-embed/src/lib.rs
+++ i18n-embed/src/lib.rs
@@ -106,6 +106,8 @@
 //! Then in your Rust code:
 //!
 //! ```
+//! # #[cfg(all(feature = "desktop-requester", feature = "fluent-system"))]
+//! # {
 //! use i18n_embed::{DesktopLanguageRequester, fluent::{
 //!     FluentLanguageLoader, fluent_language_loader
 //! }};
@@ -129,6 +131,7 @@
 //!
 //!     // continue on with your application
 //! }
+//! # }
 //! ```
 //!
 //! To access localizations, you can use `FluentLanguageLoader`'s
@@ -203,6 +206,8 @@
 //! Then in your Rust code:
 //!
 //! ```
+//! # #[cfg(all(feature = "gettext-system", feature = "desktop-requester"))]
+//! # {
 //! use i18n_embed::{DesktopLanguageRequester, gettext::{
 //!     gettext_language_loader
 //! }};
@@ -230,6 +235,7 @@
 //!
 //!     // continue on with your application
 //! }
+//! # }
 //! ```
 //!
 //! ## Automatic Updating Selection
@@ -240,6 +246,8 @@
 //! update the selected language using a [Localizer](Localizer):
 //!
 //! ```
+//! # #[cfg(all(feature = "fluent-system", feature = "desktop-requester"))]
+//! # {
 //! use std::sync::Arc;
 //! use i18n_embed::{
 //!     DesktopLanguageRequester, LanguageRequester,
@@ -281,6 +289,7 @@
 //!
 //!     // continue on with your application
 //! }
+//! # }
 //! ```
 //!
 //! The above example makes use of the
@@ -297,6 +306,8 @@
 //! you can follow this code pattern in the library itself:
 //!
 //! ```
+//! # #[cfg(feature = "fluent-system")]
+//! # {
 //! use std::sync::Arc;
 //! use i18n_embed::{
 //!     DefaultLocalizer, Localizer, LanguageLoader,
@@ -328,6 +339,7 @@
 //! pub fn localizer() -> Arc<dyn Localizer> {
 //!     Arc::new(DefaultLocalizer::new(&*LANGUAGE_LOADER, &Localizations))
 //! }
+//! # }
 //! ```
 //!
 //! People using this library can call `localize()` to obtain a
@@ -342,6 +354,8 @@
 //! for the library:
 //!
 //! ```
+//! # #[cfg(feature = "gettext-system")]
+//! # {
 //! use std::sync::Arc;
 //! use i18n_embed::{
 //!     DefaultLocalizer, Localizer, gettext::{
@@ -364,6 +378,7 @@
 //!         embed
 //!     ))
 //! }
+//! # }
 //! ```
 //!
 //! For the above example, you can enable the following options in the
@@ -419,7 +434,7 @@ pub use util::*;
 #[macro_use]
 extern crate doc_comment;
 
-#[cfg(doctest)]
+#[cfg(all(doctest, feature = "desktop-requester", feature = "fluent-system"))]
 doctest!("../README.md");
 
 #[cfg(any(feature = "gettext-system", feature = "fluent-system"))]
