c946b74a3a526a057af26a74416996f58513df0e
Luke Frisken <luke@partly.com>
2023-09-28 12:50:12 +1300

    Update cargo-i18n to clap v4, bump to v0.2.12

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use anyhow::Result;
-use clap::{crate_authors, crate_version, App, Arg};
+use clap::{builder::PossibleValuesParser, crate_authors, crate_version, Arg, Command};
 use i18n_build::run;
 use i18n_config::Crate;
 use i18n_embed::{
@@ -9,7 +9,10 @@ use i18n_embed::{
 };
 use lazy_static::lazy_static;
 use rust_embed::RustEmbed;
-use std::{path::Path, sync::Arc};
+use std::{
+    path::{Path, PathBuf},
+    sync::Arc,
+};
 use tr::tr;
 use unic_langid::LanguageIdentifier;
 
@@ -83,43 +86,46 @@ fn main() -> Result<()> {
     language_requester.add_listener(Arc::downgrade(&i18n_build_localizer_rc));
     language_requester.poll()?;
 
-    let fallback_locale = LANGUAGE_LOADER.fallback_language().to_string();
+    let fallback_locale: &'static str =
+        String::leak(LANGUAGE_LOADER.fallback_language().to_string());
     let available_languages = available_languages(&cargo_i18n_localizer_rc)?;
-    let available_languages_slice: Vec<&str> =
-        available_languages.iter().map(|l| l.as_str()).collect();
+    let available_languages_slice: Vec<&'static str> = available_languages
+        .into_iter()
+        .map(|l| String::leak(l) as &str)
+        .collect();
 
-    let matches = App::new("cargo-i18n")
+    let matches = Command::new("cargo-i18n")
         .bin_name("cargo")
         .term_width(80)
         .about(
             tr!(
                 // The message displayed when running the binary outside of cargo using `cargo-18n`.
-                "This binary is designed to be executed as a cargo subcommand using \"cargo i18n\".").as_str()
+                "This binary is designed to be executed as a cargo subcommand using \"cargo i18n\".")
         )
         .version(crate_version!())
         .author(crate_authors!())
-        .subcommand(App::new("i18n")
-            .about(short_about().as_str())
-            .long_about(long_about().as_str())
+        .subcommand(Command::new("i18n")
+            .about(short_about())
+            .long_about(long_about())
             .version(crate_version!())
             .author(crate_authors!())
             .arg(Arg::new("path")
                 .help(
                     // The help message for the `--path` command line argument.
-                    tr!("Path to the crate you want to localize (if not the current directory). The crate needs to contain \"i18n.toml\" in its root.").as_str()
+                    tr!("Path to the crate you want to localize (if not the current directory). The crate needs to contain \"i18n.toml\" in its root.")
                     )
                 .long("path")
-                .takes_value(true)
+                .num_args(1)
             )
-            .arg(Arg::new("config file name")
+            .arg(Arg::new("config-file-name")
                 .help(
                     tr!(
                         // The help message for the `-c`, `--config-file-name` command line argument.
-                        "The name of the i18n config file for this crate").as_str()
+                        "The name of the i18n config file for this crate")
                 )
                 .long("config-file-name")
                 .short('c')
-                .takes_value(true)
+                .num_args(1)
                 .default_value("i18n.toml")
             )
             .arg(Arg::new("language")
@@ -127,34 +133,34 @@ fn main() -> Result<()> {
                     tr!(
                         // The help message for the `-l`, `--language` command line argument.
                         "Set the language to use for this application. Overrides the language selected automatically by your operating system."
-                    ).as_str()
+                    )
                 )
-                .long("--language")
+                .long("language")
                 .short('l')
-                .takes_value(true)
-                .default_value(&fallback_locale)
-                .possible_values(available_languages_slice)
+                .num_args(1)
+                .default_value(&*fallback_locale)
+                .value_parser(PossibleValuesParser::new(available_languages_slice))
             )
         )
         .get_matches();
 
     if let Some(i18n_matches) = matches.subcommand_matches("i18n") {
-        let config_file_name = i18n_matches
-            .value_of("config file name")
+        let config_file_name: &String = i18n_matches
+            .get_one("config-file-name")
             .expect("expected a default config file name to be present");
 
-        let language = i18n_matches
-            .value_of("language")
+        let language: &String = i18n_matches
+            .get_one("language")
             .expect("expected a default language to be present");
-
         let li: LanguageIdentifier = language.parse()?;
+
         language_requester.set_language_override(Some(li))?;
         language_requester.poll()?;
 
         let path = i18n_matches
-            .value_of("path")
-            .map(|path_str| Path::new(path_str).to_path_buf())
-            .unwrap_or_else(|| Path::new(".").to_path_buf());
+            .get_one::<PathBuf>("path")
+            .map(ToOwned::to_owned)
+            .unwrap_or_else(|| PathBuf::from("."));
 
         let config_file_path = Path::new(config_file_name).to_path_buf();
 
