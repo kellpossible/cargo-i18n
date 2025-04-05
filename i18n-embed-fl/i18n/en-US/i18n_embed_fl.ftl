7932ee2ae9003923f13676dc65b2bc952dd3e018
Colin S <3526918+cbs228@users.noreply.github.com>
2025-04-04 21:25:20 -0500

    fix(i18n-embed-fl): check args on MessageReference
    The `fl!()` macro checks that all assigned arguments exist on
    the message in question. But fluent messages can also refer to
    other messages, and those messages can also have arguments.
    
    ```ftl
    hello = Hello { to-you }
    to-you = to you, {$name}!
    ```
    
    Previous versions of the macro could not check arguments on
    `MessageReference`s since it had no way to resolve them.
    
    Bring the owning `FluentBundle` into scope so that message
    references can be resolved. Some unnecessary generics on the
    recursive functions are made concrete.
    
    This patch requires an update to i18-embed for new API.

--- i18n-embed-fl/i18n/en-US/i18n_embed_fl.ftl
+++ i18n-embed-fl/i18n/en-US/i18n_embed_fl.ftl
@@ -4,3 +4,12 @@ hello-arg = Hello {$name}!
 hello-arg-2 = Hello {$name1} and {$name2}!
 hello-attr = Uninspiring.
     .text = Hello, attribute!
+hello-recursive = Hello { hello-recursive-descent }
+    .attr = Why hello { hello-recursive-descent }
+    .again = Why hello { hello-recursive-descent.attr }
+hello-recursive-descent = to you, {$name}!
+    .attr = again, {$name}!
+hello-select = { $attr ->
+    *[no] { hello-recursive }
+    [yes] { hello-recursive.attr }
+}
--- i18n-embed-fl/tests/fl_macro.rs
+++ i18n-embed-fl/tests/fl_macro.rs
@@ -90,3 +90,60 @@ fn with_attr_and_args() {
         fl!(loader, "hello-arg", "attr", name = "Bob")
     );
 }
+
+#[test]
+fn with_args_in_messagereference() {
+    let loader: FluentLanguageLoader = fluent_language_loader!();
+    loader
+        .load_languages(&Localizations, &[loader.fallback_language().clone()])
+        .unwrap();
+
+    pretty_assertions::assert_eq!(
+        "Hello to you, \u{2068}Bob\u{2069}!",
+        fl!(loader, "hello-recursive", name = "Bob")
+    );
+}
+
+#[test]
+fn with_args_in_messagereference_attr() {
+    let loader: FluentLanguageLoader = fluent_language_loader!();
+    loader
+        .load_languages(&Localizations, &[loader.fallback_language().clone()])
+        .unwrap();
+
+    pretty_assertions::assert_eq!(
+        "Why hello to you, \u{2068}Bob\u{2069}!",
+        fl!(loader, "hello-recursive", "attr", name = "Bob")
+    );
+}
+
+#[test]
+fn with_args_in_messagereference_attr_to_attr() {
+    let loader: FluentLanguageLoader = fluent_language_loader!();
+    loader
+        .load_languages(&Localizations, &[loader.fallback_language().clone()])
+        .unwrap();
+
+    pretty_assertions::assert_eq!(
+        "Why hello again, \u{2068}Bob\u{2069}!",
+        fl!(loader, "hello-recursive", "again", name = "Bob")
+    );
+}
+
+#[test]
+fn with_args_in_select_messagereference() {
+    let loader: FluentLanguageLoader = fluent_language_loader!();
+    loader
+        .load_languages(&Localizations, &[loader.fallback_language().clone()])
+        .unwrap();
+
+    pretty_assertions::assert_eq!(
+        "Hello to you, \u{2068}Bob\u{2069}!",
+        fl!(loader, "hello-select", attr = "", name = "Bob")
+    );
+
+    pretty_assertions::assert_eq!(
+        "Why hello to you, \u{2068}Bob\u{2069}!",
+        fl!(loader, "hello-select", attr = "yes", name = "Bob")
+    );
+}
